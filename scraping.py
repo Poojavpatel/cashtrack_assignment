259b3185f2b7fe477cca27c68ac6c3a6c6936de7
Pooja patel <poojavpatel71@gmail.com>
2018-10-23 00:59:48 +0530

    scraping all done|what is remaining

--- scraping.py
+++ scraping.py
@@ -3,23 +3,34 @@ from bs4 import BeautifulSoup
 
 f = open('dataoutput.csv', 'w', newline = '')
 writer = csv.writer(f)
-writer.writerow(['name','link','What is?','Symptoms','Causes','Diagnosis','Treatment', 'Prevention'])
+writer.writerow(['name','link','What is?','Symptoms','Causes','Diagnosis','Treatment', 'Prevention','faq'])
 
 soup = BeautifulSoup(urllib.request.urlopen("https://ada.com/conditions/"), 'html.parser')
 
 links = soup.find_all('a', attrs={"class":"s1lrc7ah-2"})
 links = [p for p in links]
 
+
+def getData(soup2, myid):
+    res=[]
+    sibs = soup2.find(attrs={"id": myid}).next_siblings
+    sibs = [sib for sib in sibs if sib != '\n']
+    for sib in sibs:
+        if(sib.name!='h2' or sib.name!='hr'):
+            res.append(sib.text.strip())
+        else:
+            break
+    res = ''.join(res)
+    return res
+
 for foo in links:
     link=foo["href"]
     soup2 = BeautifulSoup(urllib.request.urlopen(link), 'html.parser')
-    sibs = soup2.find(attrs={"id":"symptoms"}).next_siblings
-    sibs = [sib for sib in sibs if sib != '\n']
-    s2=[]
-    for sib in sibs:
-        if(sib.name != 'h2'):
-            s1.append(sib.text.strip())
-        else:
-            break
-    s2 = ''.join(s1)
-    writer.writerow([foo.text.strip(),link,'',s2,'','','',''])
+    # s1 = getData(soup2, "")
+    s2 = getData(soup2, "symptoms")
+    s3 = getData(soup2, "causes")
+    s4 = getData(soup2, "diagnosis")
+    s5 = getData(soup2, "treatment")
+    # s6 = getData(soup2, "prevention")
+    s7 = getData(soup2, "faq")
+    writer.writerow([foo.text.strip(),link,'',s2,s3,s4,s5,'',s7])
