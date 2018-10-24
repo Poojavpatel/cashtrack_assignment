359099530a78e04d5363ca2686a627b4fa215472
Pooja patel <poojavpatel71@gmail.com>
2018-10-24 11:42:33 +0530

    using passport.js for auhentication

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express');
-
+const path = require('path');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 const app = express();
 
 app.use(express.json());
 
 // Homepage route
 app.get('/',(req,res) => {
-    res.send("welcome to homepage");
+    res.sendFile(path.join(__dirname+'/static/auth.html'));
+});
+
+//  Passport setup
+app.use(passport.initialize());
+app.use(passport.session());
+app.get('/success', (req, res) => res.send("Welcome "+req.query.username+"!!"));
+app.get('/error', (req, res) => res.send("error logging in"));
+
+passport.serializeUser(function(user, cb) {
+    cb(null, user.id);
+});
+  
+passport.deserializeUser(function(id, cb) {
+    User.findById(id, function(err, user) {
+      cb(err, user);
+    });
 });
 
 port = process.env.PORT || 5000;
