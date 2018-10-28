5ea3fe2341fd573ccb865c1fe750fdbe57a2c213
Pooja patel <poojavpatel71@gmail.com>
2018-10-28 18:17:19 +0530

    added .env file

--- index.js
+++ index.js
@@ -1,31 +1,18 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const passport = require('passport');
+const multer = require("multer");
+const cloudinary = require("cloudinary");
+const cloudinaryStorage = require("multer-storage-cloudinary");
 const app = express();
 
 app.use(express.json());
+require('dotenv').config()
 
 // Homepage route
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname+'/static/auth.html'));
 });
-
-//  Passport setup
-// app.use(passport.initialize());
-// app.use(passport.session());
-// app.get('/success', (req, res) => res.send("Welcome "+req.query.username+"!!"));
-// app.get('/error', (req, res) => res.send("error logging in"));
-
-// passport.serializeUser(function(user, cb) {
-//     cb(null, user.id);
-// });
-  
-// passport.deserializeUser(function(id, cb) {
-//     User.findById(id, function(err, user) {
-//       cb(err, user);
-//     });
-// });
 
 port = process.env.PORT || 5000;
 app.listen(port,() => console.log(`Server started at port ${port}`));
